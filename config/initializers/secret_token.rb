8fc501f9c4f780c589afa07690a590560b19eb51
vagrant <vagrant@precise32.(none)>
2013-04-24 23:06:12 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scoreboard::Application.config.secret_token = '7bfec44275fbd7e9535b167f38ac6b18a3bb4aa2465cccdbe92207d58eef51051d55377845302c40a9a6c43025ea832f1e1e9bcb613d0c9746ab62c89ec234fd'
