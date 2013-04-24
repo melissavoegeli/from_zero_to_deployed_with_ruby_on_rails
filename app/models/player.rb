541c55f8283a8ac73a1b4ff49e44f5d1bd34e41a
vagrant <vagrant@precise32.(none)>
2013-04-24 23:49:08 +0000

    AR comments

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,7 @@
 class Player < ActiveRecord::Base
+  # Active Record is a query interface to your database that provides ORM
+  # Example: Player.where(:name => 'Name')
+  # This generates Select * from Player where name = 'Name';
   attr_accessible :name
 
   validates :name, :presence => true, :length => { :minimum => 2 }, :message => 'not valid'
