fcaeac2509f451069bd7aa17fb49f1c411b3eb93
vagrant <vagrant@precise32.(none)>
2013-04-25 00:18:19 +0000

    explain appcontr and flash messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
+# All controllers you generate inherit from here
+# This is a global controller that Rails automatically
+# generates for you that corresponds to app/views/layouts/application.html.erb
 class ApplicationController < ActionController::Base
   protect_from_forgery
 end
