823fe7337752f983dc8db4d7acb37e5db946a5bd
vagrant <vagrant@precise32.(none)>
2013-04-25 00:28:21 +0000

    add scoreboard form and extra methods to model

--- app/models/score_board.rb
+++ app/models/score_board.rb
@@ -1,8 +1,18 @@
 class ScoreBoard < ActiveRecord::Base
-  attr_accessible :game
+  attr_accessible :game, :player_ids
 
   validates :game, :presence => true
 
   has_many :score_cards
   has_many :players, :through => :score_cards
+
+  # the score board form is going to create
+  # score card relations on our for each
+  # player id we include
+  accepts_nested_attributes_for :players
+
+  # all of our valid players we allow
+  def initialized_players
+    Player.all
+  end
 end
